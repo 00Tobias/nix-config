bacfcbff9091ecd49cae830f61477a059de1bf59
ToxicSalt <tobiasts@protonmail.com>
2021-08-27 17:36:28 +0200

    Xorg: Update programs

--- home/xorg/programs.nix
+++ home/xorg/programs.nix
@@ -1,7 +1,21 @@
 { config, pkgs, ... }: {
   programs = {
     feh.enable = true;
-
     mpv.enable = true;
   };
+
+  services = {
+    flameshot.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    # Thunar
+    xfce.thunar
+    xfce.thunar-archive-plugin
+    xfce.thunar-volman
+    xfce.thunar-media-tags-plugin
+
+    # Plover
+    plover.dev
+  ];
 }
