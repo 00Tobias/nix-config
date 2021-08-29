3045b5529ebd2928ad84ca3bb7b6a81ee24cf994
ToxicSalt <tobiasts@protonmail.com>
2021-08-30 01:19:37 +0200

    Term: Add Alacritty config

--- home/term.nix
+++ home/term.nix
@@ -3,6 +3,57 @@
   programs = {
     alacritty = {
       enable = true;
+      settings = {
+        window = {
+          dynamic_padding = true;
+          padding = {
+            x = 0;
+            y = 0;
+          };
+        };
+        font = {
+          normal = {
+            family = "Hack Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "Hack Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "Hack Nerd Font";
+            style = "Italic";
+          };
+        };
+        colors = {
+          primary = {
+            background = "0x17191e";
+            foreground = "0xabb2bf";
+          };
+
+          normal = {
+            black = "0x1e2127";
+            red = "0xe06c75";
+            green = "0x98c379";
+            yellow = "0xd19a66";
+            blue = "0x61afef";
+            magenta = "0xc678dd";
+            cyan = "0x56b6c2";
+            white = "0x828791";
+          };
+
+          bright = {
+            black = "0x5c6370";
+            red = "0xe06c75";
+            green = "0x98c379";
+            yellow = "0xd19a66";
+            blue = "0x61afef";
+            magenta = "0xc678dd";
+            cyan = "0x56b6c2";
+            white = "0xe6efff";
+          };
+        };
+      };
     };
 
     zsh = {
@@ -55,6 +106,7 @@
     };
   };
   home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Hack" ]; })
     fd
     pfetch
     pulsemixer
