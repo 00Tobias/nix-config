617c57121e7286a4cec60465ca1d5aea22e2fcf4
ToxicSalt <tobiasts@protonmail.com>
2021-09-11 03:50:58 +0200

    Firejail: I am unsure how this option works
    This is a WIP

--- modules/firejail.nix
+++ modules/firejail.nix
@@ -6,6 +6,22 @@
         executable = "${lib.getBin pkgs.firefox}/bin/firefox";
         profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
       };
+      qutebrowser = {
+        executable = "${lib.getBin pkgs.firefox}/bin/qutebrowser";
+        profile = "${pkgs.firejail}/etc/firejail/qutebrowser.profile";
+      };
+      steam = {
+        executable = "${lib.getBin pkgs.discord}/bin/steam";
+        profile = "${pkgs.firejail}/etc/firejail/steam.profile";
+      };
+      discord = {
+        executable = "${lib.getBin pkgs.discord}/bin/discord";
+        profile = "${pkgs.firejail}/etc/firejail/discord.profile";
+      };
+      spotify = {
+        executable = "${lib.getBin pkgs.discord}/bin/spotify";
+        profile = "${pkgs.firejail}/etc/firejail/spotify.profile";
+      };
     };
   };
 }
