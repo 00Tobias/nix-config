6d3cf37942d2621badb2525cb726a75759e6bb6a
ToxicSalt <tobiasts@protonmail.com>
2021-09-24 18:29:25 +0200

    Minecraft: Add minecraft server module

---
+++ modules/mc-server.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }: {
+  services.minecraft-server = {
+    enable = true;
+    package = pkgs.papermc;
+    eula = true;
+    jvmOpts = "-Xmx4096M -Xms4096M";
+    declarative = true;
+    serverProperties = {
+      enable-jmx-monitoring = false;
+      enable-command-block = true;
+      gamemode = "survival";
+      level-name = "deez";
+      motd = "google play minecraft pocket edition";
+      pvp = true;
+      difficulty = "normal";
+      network-compression-threshold = 256;
+      max-tick-time = 60000;
+      require-resource-pack = false;
+      max-players = 69;
+      use-native-transport = true;
+      online-mode = true;
+      enable-status = true;
+      allow-flight = true;
+      broadcast-rcon-to-ops = true;
+      view-distance = 12;
+      allow-nether = true;
+      server-port = 25565;
+      sync-chunk-writes = true;
+      op-permission-level = 4;
+      prevent-proxy-connections = false;
+      entity-broadcast-range-percentage = 100;
+      player-idle-timeout = 0;
+      debug = false;
+      force-gamemode = false;
+      rate-limit = 0;
+      white-list = false;
+      broadcast-console-to-ops = true;
+      spawn-npcs = true;
+      spawn-animals = true;
+      snooper-enabled = true;
+      function-permission-level = 2;
+      spawn-monsters = true;
+      enforce-whitelist = false;
+      spawn-protection = 0;
+      max-world-size = 29999984;
+    };
+  };
+}
