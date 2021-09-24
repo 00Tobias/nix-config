02024a05bd25fef5444788cc08764afa4f2f5ef3
ToxicSalt <tobiasts@protonmail.com>
2021-09-24 18:33:13 +0200

    Qutebrowser: Big config update
    Now uses system colors

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  colors = import ./colors.nix;
+in
+{
   programs.qutebrowser = {
     enable = true;
     loadAutoconfig = true; # For notification prompts
@@ -14,11 +18,6 @@
         "*://*.reddit.com/r/*"
       ];
 
-      # Dark theme
-      colors.webpage.preferred_color_scheme = "dark";
-      colors.webpage.bg = "black";
-
-      # Adblock
       content.blocking = {
         enabled = true;
         method = "adblock";
@@ -38,24 +37,150 @@
         ];
       };
 
-      # Tabs
       tabs = {
         indicator.width = 0;
-        # padding = ''{ "bottom": 2, "left": 5, "right": 5, "top": 2 }''; # this needs to be in extraConfig, that's dumb
         title.format = "{audio}{current_title} {private}"; # {index}:
         background = true;
         show = "multiple";
         position = "right";
       };
 
-      # Statusbar
-      # statusbar.padding = ''{ "bottom": 2 "left": 1, "right": 1, "top": 2}''; # Same with this
-
-      # Downloads
       downloads.position = "bottom";
 
-      # Fonts
-      fonts.default_family = "Cozette";
+      fonts.default_family = "Hack Nerd Font";
+
+      colors = with colors.theme; {
+        webpage.preferred_color_scheme = "dark";
+
+        completion = {
+          fg = "${lighterGrey}";
+          odd.bg = "${background}";
+          even.bg = "${background}";
+          category = {
+            fg = "${blue}";
+            bg = "${background}";
+            border.top = "${background}";
+            border.bottom = "${background}";
+          };
+          item.selected = {
+            fg = "${lighterGrey}";
+            bg = "${darkGrey}";
+            border.top = "${darkGrey}";
+            border.bottom = "${darkGrey}";
+            match.fg = "${lighterGrey}";
+          };
+          match.fg = "${orange}";
+          scrollbar.fg = "${lighterGrey}";
+          scrollbar.bg = "${background}";
+        };
+
+        contextmenu = {
+          disabled.bg = "${darkerGrey}";
+          disabled.fg = "${lightGrey}";
+          menu.bg = "${background}";
+          menu.fg = "${lighterGrey}";
+          selected.bg = "${darkGrey}";
+          selected.fg = "${lighterGrey}";
+        };
+
+        downloads = {
+          bar.bg = "${background}";
+          start.fg = "${background}";
+          start.bg = "${blue}";
+          stop.fg = "${background}";
+          stop.bg = "${teal}";
+          error.fg = "${red}";
+        };
+
+        hints = {
+          fg = "${background}";
+          bg = "${yellow}";
+          match.fg = "${lighterGrey}";
+        };
+
+        keyhint = {
+          fg = "${lighterGrey}";
+          suffix.fg = "${lighterGrey}";
+          bg = "${background}";
+        };
+
+        messages = {
+          error.fg = "${background}";
+          error.bg = "${red}";
+          error.border = "${red}";
+          warning.fg = "${background}";
+          warning.bg = "${magenta}";
+          warning.border = "${magenta}";
+          info.fg = "${lighterGrey}";
+          info.bg = "${background}";
+          info.border = "${background}";
+        };
+
+        prompts = {
+          fg = "${lighterGrey}";
+          border = "${background}";
+          bg = "${background}";
+          selected.bg = "${darkGrey}";
+          selected.fg = "${lighterGrey}";
+        };
+
+        statusbar = {
+          normal.fg = "${lighterGrey}";
+          normal.bg = "${background}";
+          insert.fg = "${teal}";
+          insert.bg = "${background}";
+          passthrough.fg = "${yellow}";
+          passthrough.bg = "${background}";
+          private.fg = "${magenta}";
+          private.bg = "${background}";
+          command.fg = "${lightGrey}";
+          command.bg = "${darkerGrey}";
+          command.private.fg = "${magenta}";
+          command.private.bg = "${darkerGrey}";
+          caret.fg = "${blue}";
+          caret.bg = "${background}";
+          caret.selection.fg = "${blue}";
+          caret.selection.bg = "${background}";
+          progress.bg = "${blue}";
+          url.fg = "${lighterGrey}";
+          url.error.fg = "${red}";
+          url.hover.fg = "${orange}";
+          url.success.http.fg = "${green}";
+          url.success.https.fg = "${green}";
+          url.warn.fg = "${magenta}";
+        };
+
+        tabs = {
+          bar.bg = "${background}";
+          indicator.start = "${blue}";
+          indicator.stop = "${teal}";
+          indicator.error = "${red}";
+          odd.fg = "${lighterGrey}";
+          odd.bg = "${background}";
+          even.fg = "${lighterGrey}";
+          even.bg = "${background}";
+          pinned = {
+            even.bg = "${green}";
+            even.fg = "${background}";
+            odd.bg = "${green}";
+            odd.fg = "${background}";
+            selected = {
+              even.bg = "${darkGrey}";
+              even.fg = "${lighterGrey}";
+              odd.bg = "${darkGrey}";
+              odd.fg = "${lighterGrey}";
+            };
+          };
+          selected = {
+            odd.fg = "${lighterGrey}";
+            odd.bg = "${darkGrey}";
+            even.fg = "${lighterGrey}";
+            even.bg = "${darkGrey}";
+          };
+        };
+
+        webpage.bg = "${background}";
+      };
     };
     keyBindings = {
       normal = {
@@ -69,9 +194,20 @@
         "<Ctrl-E>" = "open-editor";
       };
     };
-    extraConfig = builtins.readFile (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/theova/base16-qutebrowser/main/themes/minimal/base16-onedark.config.py";
-      sha256 = "sha256:320a51aa400c0524ec307b2721be1267afb4fb6445e878eabcac435d879e3968";
-    });
+    extraConfig = ''
+      c.statusbar.padding = {
+        "bottom": 2,
+        "left": 1,
+        "right": 1,
+        "top": 2
+      }
+
+      c.tabs.padding = {
+        "bottom": 2,
+        "left": 5,
+        "right": 5,
+        "top": 2
+      }
+    '';
   };
 }
