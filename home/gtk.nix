b2bde32d3cfa5cedc7c83a9692aea6aabcb70c6b
ToxicSalt <tobiasts@protonmail.com>
2021-10-29 23:40:56 +0200

    GTK: Update GTK config
    Now should work better with my Sway config

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }: {
+  home.packages = [ pkgs.capitaine-cursors ];
   gtk = {
     enable = true;
+    theme = {
+      package = pkgs.materia-theme;
+      name = "Materia-dark";
+    };
     iconTheme = {
       name = "Papirus";
       package = pkgs.papirus-icon-theme;
     };
+    font = {
+      name = "Liberation Sans";
+      size = 10;
+    };
     gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
+      gtk-cursor-theme-name = "capitaine-cursors";
       gtk-decoration-layout = "menu";
     };
   };
