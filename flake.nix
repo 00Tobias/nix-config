0cb75d92fcdfb2e323748fc79191310cecbc95bb
ToxicSalt <tobiasts@protonmail.com>
2022-01-05 12:25:45 +0100

    Flake: Move Binary Cache config

--- flake.nix
+++ flake.nix
@@ -1,19 +1,6 @@
 {
   description = "Toxic's all-encompassing Nix config based on flakes";
 
-  nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://nixpkgs-wayland.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
--- hosts/common.nix
+++ hosts/common.nix
@@ -2,10 +2,14 @@
   programs.dconf.enable = true;
   nix = {
     binaryCaches = [
-      "https://nixpkgs-wayland.cachix.org"
-      "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
     ];
+    binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
     package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = nix-command flakes
