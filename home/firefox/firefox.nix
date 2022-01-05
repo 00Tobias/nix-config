39466e96da86140b98e67b23e7648bcff2edf9f1
ToxicSalt <tobiasts@protonmail.com>
2022-01-05 12:17:06 +0100

    Firefox: Cleanup config

--- home/firefox/firefox.nix
+++ home/firefox/firefox.nix
@@ -9,44 +9,11 @@
         sha256 = "nf08p9Edhug07wnaqZuptdrql5usOrzAwCLTDNmUuZs=";
       });
     };
-
-    file.".tridactylrc".text = ''
-      " Set a dark colorscheme
-      colors dark
-
-      " Swap tab switching to make it make more sense with Vertical tabs
-      bind J tabnext
-      bind K tabprev
-
-      " More Kakoune-like binds
-      bind gk scrollto 0
-      bind gj scrollto 100
-
-      " Leave Control + E to Sidebery
-      unbind <C-e>
-
-      " Don't show the mode indicator on certain sites
-      seturl youtube.com modeindicator false
-      seturl twitch.tv modeindicator false
-
-      " Don't show the mode indicator in fullscreen
-      autocmd FullscreenEnter .* set modeindicator false
-      autocmd FullscreenLeft .* set modeindicator true
-    '';
   };
 
   programs = {
     firefox = {
       enable = true;
-      # NOTE: The following causes Firefox not to build, due to some gtk3 buildInput error...
-      # Strange behaviour, have to look into that.
-      # package = pkgs.firefox.override {
-      #   cfg = {
-      #     # Tridactyl native connector
-      #     enableTridactylNative = true;
-      #   };
-      # };
-
       profiles = {
         toxic = {
           name = "toxic";
@@ -74,14 +41,11 @@
           });
           userChrome = ''
             @import "lepton/userChrome.css";
-            
+
             #TabsToolbar {
               display: none;
             }
 
-            /* Vertical tabs with Sidebery */
-
-            /* Sidebar min and max width removal */
             #sidebar {
                 max-width: none !important;
                 min-width: 0px !important;
@@ -91,20 +55,11 @@
               display: none;
             }
 
-            /* Center bookmarks toolbar */
             #PlacesToolbarItems{ -moz-box-pack: center }
           '';
           userContent = ''
             @import "lepton/userContent.css";
           '';
-        };
-        spotify = {
-          name = "spotify";
-          id = 1;
-          isDefault = false;
-          settings = {
-            "gfx.webrender.all" = true;
-          };
         };
       };
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
