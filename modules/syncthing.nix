5f8ce6500e39cc2153bd0e84c594fecb1d6d2c5d
ToxicSalt <tobiasts@protonmail.com>
2022-01-05 12:21:27 +0100

    Syncthing: Add user config

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -2,5 +2,8 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    user = "toxic";
+    dataDir = "/home/toxic/";
+    configDir = "/home/toxic/.config/syncthing";
   };
 }
