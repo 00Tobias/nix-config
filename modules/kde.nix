9b51a15e1baee3892a61a51e7b8f4ef900d49b36
ToxicSalt <tobiasts@protonmail.com>
2022-01-24 21:31:54 +0100

    KDE: Add dragon and gwenview

--- modules/kde.nix
+++ modules/kde.nix
@@ -10,6 +10,8 @@
   environment.systemPackages = with pkgs; [
     plasma-pass
     libsForQt5.ark
+    libsForQt5.dragon
+    libsForQt5.gwenview
     libsForQt5.lightly
     libsForQt5.plasma-browser-integration
     libsForQt5.plasma-nm
