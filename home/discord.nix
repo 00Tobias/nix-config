c8a9ae2b647966bea393220c4e13fa551ba4d8c7
ToxicSalt <tobiasts@protonmail.com>
2022-05-05 22:43:25 +0200

    Unfree: Split Steam and Discord into their own files

---
+++ home/discord.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }: {
+  home.packages = with pkgs; [
+    discord-canary
+  ];
+  xdg.configFile."discordcanary/settings.json".text = ''"SKIP_HOST_UPDATE": true'';
+}
--- modules/steam.nix
+++ modules/steam.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }: {
   nixpkgs.config.allowUnfree = true;
 
-  # Steam
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    freetube
-    ripcord
-    discord # For backup
-  ];
 }
