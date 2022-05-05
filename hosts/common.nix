20b1495d46e1ce998fb85056518f57bdb835f870
ToxicSalt <tobiasts@protonmail.com>
2022-05-05 23:01:24 +0200

    Hosts: Update deprecated settings

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,14 +1,16 @@
 { config, pkgs, ... }: {
   programs.dconf.enable = true;
   nix = {
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
 
     package = pkgs.nixUnstable;
     extraOptions = ''
@@ -16,7 +18,7 @@
     '';
   };
 
-  environment.systemPackages = with pkgs; [ cachix ];
+  environment.systemPackages = with pkgs; [ cachix home-manager ];
 
   fonts = {
     fonts = with pkgs; [
