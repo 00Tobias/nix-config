871991042c38590416c9b236416c40e68add44d1
ToxicSalt <tobiasts@protonmail.com>
2022-06-12 15:00:58 +0200

    Xorg: Implement very simple config (unused)

--- home/xorg/polybar.nix
+++ home/xorg/polybar.nix
@@ -1,6 +1,50 @@
-{ config, pkgs, ... }: {
-  services.polybar = {
+{ config, pkgs, ... }:
+let
+  colors = import ../colors.nix;
+in
+{
+  home.packages = [ pkgs.cozette ];
+  services.polybar = with colors.theme; {
     enable = true;
-    script = "polybar bar &"; # TODO: Change this and actually make a config lol
+    script = ''
+      polybar main &
+      polybar secondary &
+    '';
+    settings = {
+      "bar/main" = {
+        monitor = "DP-0";
+        width = "100%";
+        height = "35";
+        padding = "5";
+
+        font = [ "Cozette" ];
+        foreground = "${foreground}";
+        background = "${background}";
+
+        scroll-up = "bspwm.prev";
+        scroll-down = "bspwm.next";
+        modules-left = "bspwm";
+        modules-center = "title";
+        modules-right = "date";
+      };
+
+      "bar/secondary" = {
+        "inherit" = "bar/main";
+        monitor = "HDMI-1";
+      };
+
+      "module/bspwm" = {
+        type = "internal/bspwm";
+      };
+
+      "module/title" = {
+        type = "internal/xwindow";
+      };
+
+      "module/date" = {
+        type = "internal/date";
+        date = "%Y-%m-%d%";
+      };
+    };
   };
 }
