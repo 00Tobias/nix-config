81e3715f7df62b5cdbfc4e39391c5c2dc3f0bb34
ToxicSalt <tobiasts@protonmail.com>
2022-06-12 14:58:46 +0200

    Xorg: autologin and home-manager integration

--- modules/xorg.nix
+++ modules/xorg.nix
@@ -4,7 +4,23 @@
       enable = true;
       layout = "se";
 
-      displayManager.startx.enable = true;
+      displayManager = {
+        lightdm.enable = true;
+        # Login should be done at decryption time or not at all
+        autoLogin = {
+          enable = true;
+          user = "toxic";
+        };
+      };
+      desktopManager.session = [
+        {
+          name = "home-manager";
+          start = ''
+            ${pkgs.runtimeShell} $HOME/.hm-xsession &
+            waitPID=$!
+          '';
+        }
+      ];
 
       libinput = {
         enable = true;
