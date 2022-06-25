b9dd8062d3eb7e3d55e65a7d0d7200ee48540219
ToxicSalt <tobiasts@protonmail.com>
2022-06-25 22:47:20 +0200

    Rofi: Use theme.nix

--- home/xorg/rofi.nix
+++ home/xorg/rofi.nix
@@ -1,95 +1,89 @@
 { config, pkgs, ... }:
 let
-  colors = import ../colors.nix;
+  theme = import ../theme.nix { inherit pkgs; };
 in
 {
   programs.rofi = {
     enable = true;
-    font = "Hack 12";
-    terminal = "kitty";
+    font = with theme.font; "${name} ${toString size}";
+    # terminal = "alacritty";
     # scrollbar = false;
     # width = 40;
     # lines = 5;
     # separator = "solid";
-    pass = {
-      enable = true;
-      stores = [ "${config.xdg.dataHome}/password-store" ];
-    };
-    # mkDefault so it can be changed in sway.nix
-    extraConfig = pkgs.lib.mkDefault {
-      modi = "combi,window,drun,run";
-      combi-modi = "window,drun,run";
+    extraConfig = {
+      modi = "drun";
+      # combi-modi = "drun";
       show-icons = true;
     };
-    theme = with colors.theme;
+    theme = with theme.colors;
       let
         inherit (config.lib.formats.rasi) mkLiteral;
       in
-      {
-        "*" = {
-          background-color = mkLiteral "${background}";
-          separator-color = mkLiteral "${foreground}";
-          text-color = mkLiteral "${foreground}";
-        };
+        {
+          "*" = {
+            background-color = mkLiteral "${background}";
+            separator-color = mkLiteral "${foreground}";
+            text-color = mkLiteral "${foreground}";
+          };
 
-        "window" = {
-          padding = mkLiteral "20px";
-          height = mkLiteral "320px";
-          border = mkLiteral "3px";
-          border-color = mkLiteral "${darkGrey}";
-        };
+          "window" = {
+            padding = mkLiteral "20px";
+            height = mkLiteral "320px";
+            border = mkLiteral "3px";
+            border-color = mkLiteral "${darkGrey}";
+          };
 
-        "mainbox" = {
-          padding = mkLiteral "5px";
-          border-radius = mkLiteral "13px";
-        };
+          "mainbox" = {
+            padding = mkLiteral "5px";
+            border-radius = mkLiteral "13px";
+          };
 
-        "inputbar" = {
-          children = map mkLiteral [ "prompt" "entry" ];
-        };
+          "inputbar" = {
+            children = map mkLiteral [ "prompt" "entry" ];
+          };
 
-        "prompt" = {
-          background-color = mkLiteral "${blue}";
-          padding = mkLiteral "5px 5px 0px";
-        };
+          "prompt" = {
+            background-color = mkLiteral "${blue}";
+            padding = mkLiteral "5px 5px 0px";
+          };
 
-        "textbox-prompt-colon" = {
-          expand = false;
-          str = ":";
-        };
+          "textbox-prompt-colon" = {
+            expand = false;
+            str = ":";
+          };
 
-        "entry" = {
-          padding = mkLiteral "5px";
-        };
+          "entry" = {
+            padding = mkLiteral "5px";
+          };
 
-        listview = {
-          border = mkLiteral "0px 0px 0px";
-          padding = mkLiteral "6px 0px 0px";
-          # columns = 2;
-        };
+          listview = {
+            border = mkLiteral "0px 0px 0px";
+            padding = mkLiteral "6px 0px 0px";
+          };
 
-        "element" = {
-          padding = mkLiteral "5px";
-        };
+          "element" = {
+            padding = mkLiteral "5px";
+          };
 
-        "element-icon" = {
-          size = mkLiteral "25px";
-        };
+          "element-icon" = {
+            size = mkLiteral "25px";
+          };
 
-        "element selected" = {
-          background-color = mkLiteral "${lightGrey}";
-          text-color = mkLiteral "${foreground}";
-        };
+          "element selected" = {
+            background-color = mkLiteral "${lightGrey}";
+            text-color = mkLiteral "${foreground}";
+          };
 
-        "element urgent" = {
-          background-color = mkLiteral "${red}";
-          text-color = mkLiteral "${background}";
-        };
+          "element urgent" = {
+            background-color = mkLiteral "${red}";
+            text-color = mkLiteral "${background}";
+          };
 
-        "element active" = {
-          background-color = mkLiteral "${yellow}";
-          text-color = mkLiteral "${background}";
-        };
+          "element active" = {
+            background-color = mkLiteral "${yellow}";
+            text-color = mkLiteral "${background}";
+          };
       };
   };
 }
