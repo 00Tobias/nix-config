4063c10c6089b0dea7aaaa39f8e40f1ba54d2696
ToxicSalt <tobiasts@protonmail.com>
2022-06-25 22:49:28 +0200

    VSCodium: Update config

--- home/vscodium.nix
+++ home/vscodium.nix
@@ -2,5 +2,32 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    mutableExtensionsDir = true;
+    extensions = with pkgs.vscode-extensions; [
+      betterthantomorrow.calva
+      jnoortheen.nix-ide
+      mskelton.one-dark-theme
+      # gregoire.dance
+      # betterthantomorrow.joyride
+    ];
+
+    userSettings = {
+      editor = {
+        fontFamily = "Hack";
+        fontSize = 15;
+      };
+      workbench = {
+        colorTheme = "One Dark";
+      };
+
+      # Extensions
+      dance.modes = {
+        normal = {
+          cursorStyle = "block";
+          selectionBehavior = "character";
+        };
+      };
+      nix.enableLanguageServer = true;
+    };
   };
 }
