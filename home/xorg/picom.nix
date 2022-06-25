2c884178f1f7638bfb27fa898cd2000bcac5e324
ToxicSalt <tobiasts@protonmail.com>
2022-06-25 22:47:35 +0200

    Picom: Delete

--- home/xorg/picom.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }: {
-  services.picom = {
-    enable = true;
-    experimentalBackends = true;
-    backend = "xrender";
-    shadow = true;
-    # shadowExclude = [ "" ];
-  };
-}
